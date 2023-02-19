f63a61f1e03e93dce2ed46ee5a577dc103e3356a
SivansRawat <122634172+SivansRawat@users.noreply.github.com>
2023-02-19 12:33:25 +0530

    Added solution - LeetHub

---
+++ Make Zeroes - GFG/make-zeroes.java
@@ -0,0 +1,92 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while(T-->0)
+        {
+            String[] s = br.readLine().trim().split(" ");
+            int n = Integer.parseInt(s[0]);
+            int m = Integer.parseInt(s[1]);
+            int[][] matrix = new int[n][m];
+            for(int i = 0; i < n; i++){
+                String[] S = br.readLine().trim().split(" ");
+                for(int j = 0; j < m; j++)
+                    matrix[i][j] = Integer.parseInt(S[j]);
+            }
+            Solution ob = new Solution();
+            ob.MakeZeros(matrix);
+            for(int i = 0; i < n; i++){
+                for(int j = 0; j < m; j++){
+                    System.out.print(matrix[i][j] + " ");
+                }
+                System.out.println();
+            }
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution
+{
+    public void  MakeZeros(int[][] matrix)
+    {
+        // code here
+        
+        
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        int[][] ans = new int[rows][cols];
+        
+        for(int i=0;i<rows;i++) {
+            for(int j=0;j<cols;j++) {
+                ans[i][j] = matrix[i][j]; // copying the matrix
+            }
+        }
+        
+        for(int i=0;i<rows;i++) {
+            for(int j=0;j<cols;j++) {
+                if(matrix[i][j]==0) {
+                    int temp = 0; // this is our sum
+                    // All if conditions are to make sure that we do not go out of matrix bounds
+                    if(i-1 >= 0) { // top element
+                        temp += matrix[i-1][j]; 
+                        ans[i-1][j] = 0;
+                    }
+                    if(i+1 < rows) { // bottom element
+                        temp += matrix[i+1][j];
+                        ans[i+1][j] = 0;
+                    }
+                    if(j-1 >= 0) { // left element
+                        temp += matrix[i][j-1];
+                        ans[i][j-1] = 0;
+                    }
+                    if(j+1 < cols) { // right element
+                        temp += matrix[i][j+1];
+                        ans[i][j+1] = 0;
+                    }
+                    ans[i][j] = temp; // set current element to temp
+                }
+            }
+        }
+        for(int i=0;i<rows;i++) {
+            for(int j=0;j<cols;j++) {
+                matrix[i][j] = ans[i][j]; // copying back
+            }
+        }
+        
+       
+            
+        }
+        
+      
+      
+        
+    }

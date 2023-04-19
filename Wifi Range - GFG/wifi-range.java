bf7b9d8e85440b1dc0ba0c18048deed34eebc64b
Sivans Rawat <122634172+SivansRawat@users.noreply.github.com>
2023-04-19 12:36:18 +0530

    Added solution - LeetHub

--- Wifi Range - GFG/wifi-range.java
+++ Wifi Range - GFG/wifi-range.java
@@ -31,15 +31,9 @@ class Solution
     { 
         
         
-        boolean flag = true;
         
         for(int i=0;i<N;i++){
             
-            // if((S.substring(i-X,i+1).contains("1")) == false && (S.substring(i,i+X+1).contains("1")== false)){
-            //     flag = false;
-            // }
-            
-            // 2,5
             if(i<X){
                 
                 if((((S.substring(0,i+1)).contains("1")) == false) && (((S.substring(i,i+X+1)).contains("1")) == false)){
@@ -67,65 +61,6 @@ class Solution
         
         
         return true;
-        // int count =0;
-        
-        // for(int i=X+2;i<=N-X-1;i=i+X+2){
-            
-        //     String str = S.substring(i,i+X+3);
-        //     String str1 = S.substring(i+X+2,i+X+5);
-        //     // System.out.println(str);
-        //     if (str.contains("1")== false || str1.contains("1") == false){
-                
-        //         count++;
-        //         // System.out.print("Count:");
-        //         // System.out.println(count);
-        //         flag = false;
-        //         break;
-        //     }
-        // }
-        
-        
-        // if(S.substring(N-X-1).contains("1") == false){
-            
-        //     flag = false;
-        // }
-        
-        
-        // return flag;
-        
-        
-        
-        
-        // int i=0,j=0;
-        
-        // while(j<N){
-            
-        //     if(j-i+1<(X+1)){
-                
-        //         j++;
-        //     }
-            
-            
-        //     else if(j-i+1 ==(X+1)){
-                
-        //         if(S.substring(i,j+1).contains("1") ==  false){
-                    
-        //             flag = false;
-        //             break;
-                    
-        //         }
-        //         j++;
-        //         i++;
-        //     }
-        // }
-        
-        // return flag;
-        
-        
-        
-        
-        
-        
-        // code here
+       
     }
 } 

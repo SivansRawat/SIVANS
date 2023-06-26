5c0a90402cd8a583b6251fb40ec3bb3c8cd4087c
Sivans Rawat <122634172+SivansRawat@users.noreply.github.com>
2023-06-26 17:16:35 +0530

    Added solution - LeetHub

--- Key Pair - GFG/key-pair.java
+++ Key Pair - GFG/key-pair.java
@@ -41,18 +41,20 @@ class Solution {
         
         
         Map<Integer,Integer> map = new HashMap<Integer,Integer>();
-        
-        for(int i =0 ;i<n;i++){
-            
-            if(map.containsKey(x-arr[i])){
+
+        for(int i = 0 ; i<arr.length;i++){
+
+            if(map.containsKey(x - arr[i])){
                 
-                return true ;
+                return true;
+                
             }
             
+            
             map.put(arr[i] , i);
+        
         }
-        
-        
-        return false;
+
+        return false ;
     }
 }

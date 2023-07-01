8c0d0dc3d8caa7c34ed901f766e910fc8093549a
Sivans Rawat <122634172+SivansRawat@users.noreply.github.com>
2023-07-01 09:58:51 +0530

    Added solution - LeetHub

--- Number of 1 Bits - GFG/number-of-1-bits.java
+++ Number of 1 Bits - GFG/number-of-1-bits.java
@@ -25,14 +25,14 @@ class Solution {
     static int setBits(int N) {
         // code here
         
+        
         int count = 0, mask = 1;
         for(int i = 0; i < 32; i++){
             if((N & mask) != 0) count++;
+            
+            // left shift 1 every time while running for loop if at that index 1 comes so we add count++
             mask = mask << 1;
         }
         return count;
-        
-        
-        
     }
 }

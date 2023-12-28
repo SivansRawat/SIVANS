acd7105cebcd83bbe58eb0d61d63c52113769410
Sivans Rawat <122634172+SivansRawat@users.noreply.github.com>
2023-12-28 23:55:05 +0530

    Added solution - LeetHub

--- Find the number of islands - GFG/find-the-number-of-islands.java
+++ Find the number of islands - GFG/find-the-number-of-islands.java
@@ -28,84 +28,45 @@ class GFG {
 
 
 
-class Pair{
-    
-    int first;
-    int second;
-    
-    public Pair(int first,int second){
-        
-        this.first = first;
-        this.second = second;
-    }
-}
-
-
-
 class Solution {
-    
-    
-    
-    
-    public  void bfs(int ro,int co,int[][]vis,char[][]grid){
-        
-        vis[ro][co] = 1;
-        Queue<Pair> q = new LinkedList<Pair>();
-        
-        q.add(new Pair(ro,co));
-        
-        int  n = grid.length;
-        int  m = grid[0].length;
-        
-        while(!q.isEmpty()){
-            
-            int row = q.peek().first;
-            int col = q.peek().second;
-            q.remove();
-            
-            // Checking the neighbouring elements 
-            for(int delrow = -1 ;delrow <= 1;delrow++){
-                for(int delcol = -1;delcol<=1;delcol++){
-                    
-                    int nrow = row+delrow;
-                    int ncol = col + delcol;
-                    
-                    
-                    if(nrow >=0 && nrow < n && ncol >=0 && ncol<m && grid[nrow][ncol] == '1' && vis[nrow][ncol] == 0 ){
-                        
-                        // Mark as particular index in visited array as visited 
-                        vis[nrow][ncol] = 1;
-                        // then add the particular element in the queue 
-                        q.add(new Pair(nrow,ncol));
-                    } 
-                }
-            }
-        }
-    }
-    // Function to find the number of islands.
-    public int numIslands(char[][] grid) {
-     
-        // Code here
-        
+
+    public void dfs(int row, int col, int[][] vis, char[][] grid) {
         int n = grid.length;
         int m = grid[0].length;
-        
-        int [][] vis = new int[n][m];
-        int cnt =0 ;
-        
-        for(int row =0;row<n;row++){
-            for(int col =0;col<m;col++){
-                // VImp condition to only bfs it if the given node is unvisited 
-                if(vis[row][col] == 0 && grid[row][col] == '1'){
-                    
+
+        if (row < 0 || row >= n || col < 0 || col >= m || grid[row][col] == '0' || vis[row][col] == 1) {
+            return;
+        }
+
+        vis[row][col] = 1;
+
+        // Explore neighbors in all eight directions
+        int[] dr = {-1, -1, -1, 0, 0, 1, 1, 1};
+        int[] dc = {-1, 0, 1, -1, 1, -1, 0, 1};
+
+        for (int i = 0; i < 8; i++) {
+            int newRow = row + dr[i];
+            int newCol = col + dc[i];
+            dfs(newRow, newCol, vis, grid);
+        }
+    }
+
+    public int numIslands(char[][] grid) {
+        int n = grid.length;
+        int m = grid[0].length;
+
+        int[][] vis = new int[n][m];
+        int cnt = 0;
+
+        for (int row = 0; row < n; row++) {
+            for (int col = 0; col < m; col++) {
+                if (vis[row][col] == 0 && grid[row][col] == '1') {
                     cnt++;
-                    // bfs traversal 
-                    bfs(row,col,vis,grid);
+                    dfs(row, col, vis, grid);
                 }
             }
         }
-        
+
         return cnt;
-        
     }
 }
